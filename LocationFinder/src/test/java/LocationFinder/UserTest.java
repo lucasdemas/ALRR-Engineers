bf86b5af71d9f2992326b6a24d57787f914fdd40
atheer <asa2259@columbia.edu>
2022-10-17 13:54:35 -0500

    testing

--- LocationFinder/src/test/java/LocationFinder/UserTest.java
+++ LocationFinder/src/test/java/LocationFinder/UserTest.java
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-
-import LocationFinder.repositories.UserRepository;
-import LocationFinder.models.User;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class UserTest {
-
-    @Autowired
-    private UserService userService;
-
-
-    // Test that the id is correctly updated by postUser method
-    @Test
-    public void testAddUser() {
-
-        //Test adding a new user 
-        assertEquals(addNewUser("atheer_test@gmail.com","atheer123"), "Saved");
-
-    }
-
-}
-
-=======
 package LocationFinder;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -50,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.Mockito;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -64,13 +25,20 @@ class UserTest {
 
     @Test
     void addNewUser() {
-        String returnedString = userCon.addNewUser("test4", "test4@test.com");
-        assertEquals("Saved", returnedString);
+        //String returnedString = userCon.addNewUser("test4", "test4@test.com");
+        //assertEquals("Saved", returnedString);
+
+        User n = new User();
+        n.setName("test_atheer1");
+        n.setEmail("test_atheer1@gmail.com");
+
+        Mockito.when(userRepo.save(n)).thenReturn(n);
+
     }
 
     @Test
     void getAllUsers() {
 
+
     }
 }
->>>>>>> ad0a8a8f459546afd4ddf57b36a2203c4ed312c3
