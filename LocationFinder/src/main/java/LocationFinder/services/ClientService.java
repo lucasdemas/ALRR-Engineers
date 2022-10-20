1ec0235e4dbaccf9b2e86accb5ee27e407b24cc5
Lucas Demas <lucasdemas@yahoo.com>
2022-10-20 17:26:36 -0400

    Worked with Ryan to add functionality to add a new client and get all of the clients back

--- LocationFinder/src/main/java/LocationFinder/services/ClientService.java
+++ LocationFinder/src/main/java/LocationFinder/services/ClientService.java
@@ -1,5 +1,6 @@
 package LocationFinder.services;
 
+import LocationFinder.exceptions.InvalidTypeException;
 import LocationFinder.models.Client;
 import LocationFinder.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,4 +14,16 @@ import java.util.Optional;
 public class ClientService {
     @Autowired
     private ClientRepository clientRepo;
+
+    public Client addClient(Client client) {
+        clientRepo.save(client);
+        return client;
+    }
+
+    public void checkInvalid(Client client) throws InvalidTypeException {
+        if (client.getName().trim().isEmpty()){
+            throw new InvalidTypeException("Client name cannot be blank");
+        }
+    }
+
 }
