438a88bbea64f959dd8f21c5a02397f70089a4e1
Lucas Demas <lucasdemas@yahoo.com>
2022-10-23 12:05:33 -0400

    Added comments to tests and cleaned up random/undeeded code/comments

--- LocationFinder/src/test/java/LocationFinder/ClientTest.java
+++ LocationFinder/src/test/java/LocationFinder/ClientTest.java
@@ -6,10 +6,8 @@ import LocationFinder.exceptions.InvaildInputException;
 import LocationFinder.models.Client;
 import LocationFinder.repositories.ClientRepository;
 import LocationFinder.services.ClientService;
-import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +17,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import LocationFinder.exceptions.NotFoundException;
 import java.util.Optional;
 
-
-
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class ClientTest {
@@ -30,8 +25,6 @@ class ClientTest {
 
     @MockBean
     private ClientRepository clientRepo;
-
-//Figure out catch exception junit 5
 
     //Test for attempting to delete a client who does not exist (no client with the specified client id)
     @Test
@@ -51,26 +44,35 @@ class ClientTest {
     //Test to successfully add a client to the database
     @Test
     public void testAddClient() {
-
+        //Create a mock client that will represent the data that would be passed in for when a new client is added
         Client client1 = new Client(null, "Client Test", "ClientTest@client.com");
 
+        //Create a second mock client to represent that the mock client will be given the client id 100 when added to the repo
         Client client2 = new Client(100, "Client Test", "ClientTest@client.com");
 
+        //Have the mock return the second mock client when it will save the first one to the client repo
         Mockito.when(clientRepo.save(client1)).thenReturn(client2);
+
+        //Check to see that the fist mock client's id was updated to 100 after being added to the client repo
         assertEquals(clientServ.addClient(client1).getId(), 100);
     }
 
     //Test to successfully get a specific client based on the client id
     @Test
     public void testGetClientById() throws NotFoundException {
-
-
+        //Create a mock client who we will search for by their id
         Client client1 = new Client(100, "Client Test", "ClientTest@client.com");
+
+        //Have the client be returned in the format that findById is looking for in the cleintRepo
         Optional<Client> optClient = Optional.of(client1);
+
+        //Have the mock return the formatted client when it look for a client with the id 100
         Mockito.when(clientRepo.findById(100)).thenReturn(optClient);
 
+        //Get the result of searching for a client with the id 100
         Client clientResult = clientServ.getClientById(100);
 
+        //Check to see that the results of the service returned the correct data
         assertEquals(clientResult.getId(), 100);
         assertEquals(clientResult.getName(), "Client Test");
         assertEquals(clientResult.getEmail(), "ClientTest@client.com");
@@ -94,23 +96,25 @@ class ClientTest {
     //Test to successfully update a client's email
     @Test
     public void testUpdateClientEmail() throws NotFoundException {
-
-
+        //Create a mock client who's email we will update
         Client client1 = new Client(100, "Client Test", "ClientTest@client.com");
 
-        //Optional<Client> optClient = Optional.of(client1);
-        //Mockito.when(clientRepo.findById(100)).thenReturn(optClient);
-
+        //Have the client be returned in the format that findById is looking for in the cleintRepo
         Optional<Client> optClient = Optional.of(client1);
+
+        //Have the mock return the formatted client when it look for a client with the id 100
         Mockito.when(clientRepo.findById(100)).thenReturn(optClient);
 
+        //Have the mock return true when it checks to see if there is a client with the id 100 in the repo
         Mockito.when(clientRepo.existsById(100)).thenReturn(true);
 
-        //Mockito.when(clientServ.getClientById(100)).thenReturn(client1);
+        //Save the client to the repository
         Mockito.when(clientRepo.save(client1)).thenReturn(client1);
 
+        //Update the mock client's email
         Client clientResult = clientServ.updateClientEmail(100,"UpdatedClientTest@client.com");
 
+        //Check to see that the client's email was updated successfully
         assertEquals(clientResult.getEmail(), "UpdatedClientTest@client.com");
     }
 
@@ -140,19 +144,6 @@ class ClientTest {
             }
         });
     }
-
-
-
-
-    // Testing to see if we can add a new location
-//    @Test
-//    public void addNewLocTest() {
-//        Location loc = new Location("Mudd Building", "NYC", 30.50);
-//        // Mockito.when(locRepo.save(loc)).thenReturn(loc);
-//        // System.out.println(loc.getName());
-//        String result = locController.addNewLoc("Mudd Building", "NYC", 30.50);
-//        assertEquals("Saved", result);
-//    }
 }
 
 
