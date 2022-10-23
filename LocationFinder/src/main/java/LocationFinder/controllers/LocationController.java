4562fd82a960fef57fa2fb416cbe05aa73ab0456
grossmann4 <grossmann.4@yahoo.com>
2022-10-23 15:21:55 -0400

    added javadoc comments to location controller with Ruiyang

--- LocationFinder/src/main/java/LocationFinder/controllers/LocationController.java
+++ LocationFinder/src/main/java/LocationFinder/controllers/LocationController.java
@@ -16,13 +16,24 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/location")
 public class LocationController {
+    /**
+     * An instance of Location Repository.
+     */
     @Autowired
     private LocationRepository locRepository;
-
+    /**
+     * An instance of Location Service.
+     */
     @Autowired
     private LocationService locService;
 
-    //Exception handling for InvalidTypeInput
+    /**
+     * Exception handling for InvalidTypeInput.
+     * @param e
+     *      The Number Format Exception that is going to be handled
+     * @return
+     *      The response for inputting a negative value for cost
+     */
     @ExceptionHandler(NumberFormatException.class)
     public ResponseEntity<?> handleInvalidNumber(
         final NumberFormatException e) {
@@ -33,7 +44,18 @@ public class LocationController {
 
 
 
-    //Call to add a new location to the database
+    /**
+     * A method that adds a new location to the database.
+     * @param loc_name
+     *      The location name
+     * @param loc_area
+     *      The location area
+     * @param loc_cost
+     *      The location cost
+     * @return
+     *      The response for a successfully created location
+     *      or the response for a caught exception
+     */
     @PostMapping(path = "/add")
     public ResponseEntity<?> addNewLoc(@RequestParam final String loc_name,
                     @RequestParam final String loc_area,
@@ -65,21 +87,38 @@ public class LocationController {
 
     }
 
-    //Call to get back all of the location in the database
+    /**
+     * A method to get all locations in the database.
+     * @return
+     *      The response from finding all the locations in the database.
+     */
     @GetMapping(path = "/getAll")
     public ResponseEntity<?> getLocations() {
         return new ResponseEntity<>(locRepository.findAll(), HttpStatus.OK);
     }
 
-    //Call to get all of the locations from a specific area
+    /**
+     * A method to get all locations in the database by
+     * which area they are located in.
+     * @param area
+     *      The area where the desired locations are
+     * @return
+     *      The response from finding all locations in the given area
+     */
     @GetMapping(path = "/get/{area}")
     public ResponseEntity<?> getLocByArea(@PathVariable final String area) {
         return new ResponseEntity<>(locRepository.findByArea(area),
          HttpStatus.OK);
     }
 
-    //Call to get all of the locations that are either
-    //claimed or unclaimed given the user's input
+    /**
+     * A method to get all locations by claimed status.
+     * @param isClaim
+     *      The claimed status on which to search the database
+     * @return
+     *      The response from finding all locations by claimed
+     *      status or the response from catching an exception
+     */
     @GetMapping(path = "/getClaim/{isClaim}")
     public ResponseEntity<?> getLocByClaim(@PathVariable final String isClaim) {
         try {
@@ -92,7 +131,16 @@ public class LocationController {
         }
     }
 
-    //Call to update the cost of a location
+    /**
+     * A methodto update the cost of an already existing location.
+     * @param loc_id
+     *      The id of the location in need of changing
+     * @param loc_cost
+     *      The new cost of the location
+     * @return
+     *      The response from updating the cost or the response
+     *      from the location not existing
+     */
     @PostMapping(path = "/updateCost")
     public ResponseEntity<?> updateLocCost(@RequestParam final Integer loc_id,
                                 @RequestParam final Double loc_cost) {
@@ -110,7 +158,16 @@ public class LocationController {
         }
     }
 
-    //Call to update the claim status of a given location
+    /**
+     * A method to update the claimed status of an existing location.
+     * @param loc_id
+     *      The id of the location in need of updating
+     * @param loc_claim
+     *      The new claimed status of the location
+     * @return
+     *      The response from updating the claimed status or the
+     *      response from the location not existing
+     */
     @PostMapping(path = "/updateClaim")
     public ResponseEntity<?> updateLocClaim(@RequestParam final Integer loc_id,
                                 @RequestParam final Boolean loc_claim) {
@@ -128,7 +185,14 @@ public class LocationController {
         }
     }
 
-    //Call to delete a location from the database
+    /**
+     * A method to delete an existing location.
+     * @param loc_id
+     *      The id of the location to be deleted
+     * @return
+     *      The response from successfully deleting the location or
+     *      the response from the location not existing
+     */
     @PostMapping(path = "/delete")
     public ResponseEntity<?> deleteLoc(@RequestParam final Integer loc_id) {
         try {
