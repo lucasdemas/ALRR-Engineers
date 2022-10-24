363af2a32c4b45ed9964710b3c801ee0782183a2
grossmann4 <grossmann.4@yahoo.com>
2022-10-24 16:05:37 -0400

    Finished style checking

--- LocationFinder/src/main/java/LocationFinder/LocationFinderApplication.java
+++ LocationFinder/src/main/java/LocationFinder/LocationFinderApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LocationFinderApplication {
-
+	/**
+	 * Run the spring application.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(LocationFinderApplication.class, args);
 	}
