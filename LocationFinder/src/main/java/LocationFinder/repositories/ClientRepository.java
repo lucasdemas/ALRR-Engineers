4dcf832b79d0e1d71301550e268c4f6ca6c3882c
Ruiyang Hu <huruiyangsam@gmail.com>
2022-11-27 18:25:37 -0500

    Fix style issue for ClientRepository.java

--- LocationFinder/src/main/java/LocationFinder/repositories/ClientRepository.java
+++ LocationFinder/src/main/java/LocationFinder/repositories/ClientRepository.java
@@ -13,23 +13,28 @@ import java.util.Optional;
 public interface ClientRepository extends CrudRepository<Client, Integer> {
 
     /**
-     * Query to find a client in the database by their authentication token (unencrypted).
+     * Query to find a client in the database
+     * by their authentication token (unencrypted).
      * @param clientAuthToken
      * @return
      *      List of clients
      */
-    @Query(value = "select * from client where client_auth_token = :clientAuthToken", nativeQuery = true)
-    Optional<Client> findByAuthToken(@Param("clientAuthToken") String clientAuthToken);
+    @Query(value = "select * from client where client_auth_token "
+            + "= :clientAuthToken", nativeQuery = true)
+    Optional<Client> findByAuthToken(@Param("clientAuthToken")
+                                     String clientAuthToken);
 
 
     /**
-     * Query to see if the auth token is already in use by a client in the database
+     * Query to see if the auth token is already in use
+     * by a client in the database.
      * @param clientAuth
      * @return
-     *      True/False depending if there is a client with the auth token or not
+     *      True/False depending on
+     *      if there is a client with the auth token or not
      */
-    @Query("select case when count(c)> 0 then true else false end from Client c where lower(c.authToken) like lower(:clientAuth)")
+    @Query("select case when count(c)> 0 then true else false end from "
+            + "Client c where lower(c.authToken) like lower(:clientAuth)")
     boolean existsByAuth(@Param("clientAuth") String clientAuth);
-
 
 }
